98e3169b59bee8e7cab28ce1f14beb8615e81192
Romka023 <meshkov.ra@yandex.ru>
2018-11-22 17:11:29 +0300

    - Changed most of colors on the page

--- index.php
+++ index.php
@@ -10,6 +10,26 @@
     <link rel="shortcut icon" href="assets/img/favicon.png" type="image/png">
     <link href="assets/css/lib/bootstrap4_custom.css" rel="stylesheet">
     <link href="assets/css/style.css" rel="stylesheet">
+    <script type="text/javascript">!function (e, t, a) {
+            (t[a] = t[a] || []).push(function () {
+                try {
+                    t.yaCounter51162917 = new Ya.Metrika2({
+                        id: 51162917,
+                        clickmap: !0,
+                        trackLinks: !0,
+                        accurateTrackBounce: !0
+                    })
+                } catch (e) {
+                }
+            });
+            var c = e.getElementsByTagName("script")[0], n = e.createElement("script"), r = function () {
+                c.parentNode.insertBefore(n, c)
+            };
+            n.type = "text/javascript", n.async = !0, n.src = "https://mc.yandex.ru/metrika/tag.js", "[object Opera]" == t.opera ? e.addEventListener("DOMContentLoaded", r, !1) : r()
+        }(document, window, "yandex_metrika_callbacks2");</script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/51162917" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
 </head>
 <body>
 <header class="container-fluid headerWrapper">
